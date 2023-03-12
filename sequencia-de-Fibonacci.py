72f78e47f12be05b6281131876d9cc2b36303bf9
AmandaSilva <amandasilvalopes.dev@gmail.com>
2023-03-12 19:38:52 -0300

    resposta

---
+++ sequencia-de-Fibonacci.py
@@ -0,0 +1,18 @@
+
+# num = int(input("Digite o numero que deseja pesquisar: "))
+# n1 = 0
+# n2 = 1
+# n3 = n1 + n2
+# while n3 <= num:
+#     if n3 == num:
+#         print(num, "pertence sequência de Fibonacci")
+#         break
+#     n1 = n2
+#     n2 = n3
+#     n3 = n1 + n2
+# else:
+#     print(num, "não pertence sequência de Fibonacci")
+
+
+entrada = input()
+print(entrada[::-1])
